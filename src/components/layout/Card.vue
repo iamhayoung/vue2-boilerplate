6ad7f6cc9769b951f50b46a9bc271617c67aed0e
Hayoung Gam <hahhyoung@gmail.com>
2021-08-13 21:13:10 +0900

    Add: SCSS 설치, Reset, Variables, Mixin.scss 생성 및 적용

---
+++ src/components/layout/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">card</div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  @include flex(center, center);
+  background: green;
+}
+</style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,9 +1,24 @@
 <template>
-  <header>Header!</header>
+  <div>
+    <header>Header!</header>
+    <Card />
+  </div>
 </template>
 
 <script>
+import Card from './Card';
+
 export default {
   name: 'Header', // 이 컴포넌트의 이름은 header다
+  components: {
+    Card, // 이안에서 연결해줌
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+header {
+  background: black;
+  color: white;
+}
+</style>
